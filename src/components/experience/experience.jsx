661d47ea319364a70a9361b078a8c4af9ca6809d
Chakri-iitg <140934794+Chakri-iitg@users.noreply.github.com>
2023-11-20 22:32:25 +0530

    Update experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -87,7 +87,7 @@ const experience = () => {
                 <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon"/>
                    <div>
-                        <h4>React</h4>
+                        <h4>Express</h4>
                         <small className='text-light'>Experienced</small>
                    </div>
                 </article>
